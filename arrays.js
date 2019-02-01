bad65cacaa4f5ef279bbfa11b5d5e14a9f28c963
Antonina Maj <antoninajanina@gmail.com>
2019-02-01 15:12:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,19 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array,element) {
-  arrayNew = [element, ...array]
+  var arrayNew = [element, ...array]
   return arrayNew
 }
 
 function destructivelyAddElementToBegginingOfArray(array,element) {
   return unshift.array(element)
+}
+
+function addElementToEndOfArray(array,element) {
+  var arrayNew = [...array, element]
+  return arrayNew
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  return push.array
 }
