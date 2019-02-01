63f250f1e8212fa4c115e06bbf81b02f66044926
Antonina Maj <antoninajanina@gmail.com>
2019-02-01 15:41:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,3 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array) {
+  var newArray = array.slice(0,array.length)
+}
